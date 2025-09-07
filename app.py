cd1f3ddd03966dba04ef74f2134a840a60a670d7
Mehakpreet Kaur <mehakpreet.kaur2512@gmail.com>
2025-09-07 12:39:44 +0530

    app.py

--- app.py
+++ app.py
@@ -3,21 +3,22 @@ from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import jarvis_logic
 
-
 app = Flask(__name__)
 CORS(app)
 
+# Root route (homepage)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Chat API
 @app.route('/chat', methods=['POST'])
 def chat():
     data = request.get_json()
     user_message = data.get("message")
     assistant_reply = jarvis_logic.get_response(user_message)
     return jsonify({'response': assistant_reply})
-@app.route('/')
-def home():
-    return render_template('index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
